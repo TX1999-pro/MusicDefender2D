a25d2881f63dc2fc8a284c83dd0b68500ad2cd3e
Tianxiao <tw1119@ic.ac.uk>
2023-03-12 00:25:36 +0000

    OSC message receiver added
    update the player position in game by OSC message received

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,17 +1,28 @@
+using extOSC;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
     public float targetPosition;
     public float leftBound;
     public float rightBound;
+    public string address;
+
     [SerializeField] private GameObject player;
     [SerializeField] private float speed;
+    [SerializeField] private OSCReceiver receiver;
+
+    
     void Start()
     {
-
+        address = "/player/position";
+        // get the receiver
+        receiver = GetComponent<OSCReceiver>();
+        receiver.Bind(address, MessageReceived);
     }
 
     // Update is called once per frame
@@ -33,6 +44,22 @@ public class PlayerController : MonoBehaviour
             inBoundPosition.x = rightBound;
         }
         transform.position = Vector2.MoveTowards(transform.position, inBoundPosition, speed * Time.deltaTime);
+    }
+
+    void MessageReceived(OSCMessage message)
+    {
+        // check if the received message is of the right
+        Debug.Log("message received from " + address);
+        Debug.Log(message);
+
+        if (message.ToFloat(out var value))
+        {
+            targetPosition = value;
+            Debug.Log(value);
+        } else
+        {
+            Debug.Log("Message type error. Please make sure a float is sent.");
+        }
     }
 
 }
